49017d130879bbc01c3f50b784031b84c1b6946b
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-31 13:24:48 +0100

    create empty array to use them to pick in panel

--- main.js
+++ main.js
@@ -3,11 +3,18 @@ const player = 'far fa-circle';
 const cpu = 'far fa-times-circle';
 let round = 1;
 
+const board = [
+    [" " ," " ," "],
+    [" " ," " ," "],
+    [" " ," " ," "]
+]
+
 const panels = [...document.querySelectorAll('.panel')];
 
 panels.forEach(panel => panel.addEventListener("click", createClick));
 
 const createClick = (event) => {
     const turn = round % 2 === 0 ? player : cpu;
-event.target.classList.add(turn)
+    const {row , column} = event.target.dataset;
+       event.target.classList.add(turn);
 }
