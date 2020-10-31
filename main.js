8335a2f2493983496768b123e14a5f02679ecbe9
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-31 13:26:14 +0100

    create array with ways to pick in the panel to sum

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ const board = [
     [" " ," " ," "]
 ]
 
+const aWays = [
+    [" 0 " ," 1 " ," 2 "],
+    [" 3 " ," 4 " ," 5 "],
+    [" 6 " ," 7 " ," 8 "]
+]
+
 const panels = [...document.querySelectorAll('.panel')];
 
 panels.forEach(panel => panel.addEventListener("click", createClick));
