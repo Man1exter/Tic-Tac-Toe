b0db1b7329a8b8ad11881362de672a51b33344e2
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-01 17:19:05 +0100

    cgane to global variables to declar

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-const panels = document.querySelectorAll('.panel');
+const $panels = document.querySelectorAll('.panel');
 
+
+const pick = (event) => {
+
+const itemX = document.createElement('<i class="far fa-circle"></i>');
+const itemY = document.createElement('<i class="far fa-times-circle"></i>');
+
+
+
+panels.appendChild(itemX);
+panels.appendChild(itemY);
+};
+
+
+
+
+
+$panels.addEventListener('click', pick);
 
 
 
