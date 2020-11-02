7d548a4a314126dc479ec4820f2632e74c5e84f1
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-02 09:06:53 +0100

    add empty combition and panels to win effect

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 
-
 const $panels = document.querySelectorAll('.panel');
 const $panelsEle = [...$panels];
 
 const $infoP = document.querySelector('.info');
 
 let round = 1;
+let playerX = 'X';
+
+let combinationsEmpty = ["","","","","","","","",""];
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 
 const pickItem = (e) => {
