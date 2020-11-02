2524bd7524fd4b6e615f6fcfedbdcef441baab65
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-02 17:42:39 +0100

    create main function with other fct in them

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 
-const iconX = ` <i class="far fa-circle"></i>`;
-const iconO = `<i class="far fa-times-circle"></i>`
+const gameSet = new gameSet();
+gameSet.start();
+
+function gameSet(){
+    const board = new board();
+    const playerX = new playerX();
+    const cpuY = new cpuY();
+}
+
+
 
 
 
