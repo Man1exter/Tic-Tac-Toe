32b7ec5e943aa57608ab068bea8830fa8dc037b7
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-02 17:46:28 +0100

    creeate elements with OOP metod to do finish

--- main.js
+++ main.js
@@ -3,10 +3,26 @@ const gameSet = new gameSet();
 gameSet.start();
 
 function gameSet(){
-    const board = new board();
-    const playerX = new playerX();
-    const cpuY = new cpuY();
-}
+    const board = new Board();
+    const playerX = new PlayerX();
+    const cpuY = new CpuY();
+
+    this.start = function(){
+
+    }
+};
+
+function Board(){
+
+} ;
+
+function PlayerX(){
+
+};
+
+function CpuY(){
+
+};
 
 
 
