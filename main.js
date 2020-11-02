7e0b8b7d60fbf1c72868cdea9d96e1874d6a06a3
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-02 09:00:53 +0100

    create in css grid template and elelemtns on them

--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 
 const $panels = document.querySelectorAll('.panel');
 const $panelsEle = [...$panels];
+
 const $infoP = document.querySelector('.info');
+
 let round = 1;
 
 
 const pickItem = (e) => {
+if(round % 2 === 0 ){
 
-
+}
 
 
 }
