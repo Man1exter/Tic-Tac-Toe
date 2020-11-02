163256b7270dc43422108c129dfd3e36fb5de73e
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-02 08:33:16 +0100

    create new assigments to create fnctions

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+
+
 const $panels = document.querySelectorAll('.panel');
+const $panels = [...('.panel')];
+let round = 1;
 
 
 const pick = (event) => {
@@ -6,10 +10,13 @@ const pick = (event) => {
 const itemX = document.createElement('<i class="far fa-circle"></i>');
 const itemY = document.createElement('<i class="far fa-times-circle"></i>');
 
+if(event.target.value !== ' '){
+
+}
 
 
-panels.appendChild(itemX);
-panels.appendChild(itemY);
+$panels.appendChild(itemX);
+$panels.appendChild(itemY);
 };
 
 
